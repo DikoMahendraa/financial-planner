681300b8d12e12ba3494ca4974e0bf75e61049fa
Diko Mahendra <macbookair@MacBooks-MacBook-Air.local>
2023-07-23 17:55:36 +0700

    checking husky

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import type { AppProps } from "next/app";
-import "@/globals.css";
+import type { AppProps } from 'next/app';
+import '@/globals.css';
 
 export default function MyApp({ Component }: AppProps) {
   return <Component {...pageProps} />;
