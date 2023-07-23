91913d8d216c9b09030ddfa4ab0ac2bf7845ebba
Diko Mahendra <macbookair@MacBooks-MacBook-Air.local>
2023-07-23 18:04:30 +0700

    checking husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="bg-red-200">
-      <h1>Hello world</h1>
+      <h1>Hello dasdasdd world</h1>
     </div>
   );
 }
