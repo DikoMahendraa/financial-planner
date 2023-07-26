655ca2e7f37b8ae5f24a562150fb9082d704359b
Diko Mahendra <macbookair@MacBooks-MacBook-Air.local>
2023-07-26 21:38:30 +0700

    add: home screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,35 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="bg-red-200">
-      <h1>Hello dasdasdd world</h1>
-    </div>
+    <main className="h-screen">
+      <div className="md:max-w-[360px] w-full h-full m-auto relative bg-[#CEF1E5]">
+        <section className=" bg-blue-400 px-5">card</section>
+
+        <section className="absolute bottom-5 w-full px-4">
+          <div className="bg-black h-18 w-full rounded-[40px] grid grid-cols-5 p-1 space-x-1">
+            <div className="col-span-2 cursor-pointer">
+              <div className="w-full h-16 rounded-full bg-blue-400 flex items-center justify-center">
+                <p className="text-white m-0">Home</p>
+              </div>
+            </div>
+            <div className="col-span-1 ">
+              <div className="w-full h-full rounded-full bg-blue-400 flex items-center justify-center">
+                <p className="text-white m-0">E</p>
+              </div>
+            </div>
+            <div className="col-span-1">
+              <div className="w-full h-full rounded-full bg-blue-400 flex items-center justify-center">
+                <p className="text-white m-0">I</p>
+              </div>
+            </div>
+            <div className="col-span-1">
+              <div className="w-full h-full rounded-full bg-blue-400 flex items-center justify-center">
+                <p className="text-white m-0">S</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 }
