332f51e57f6b3b7aee5e122e663ae95de85fa38e
Diko Mahendra <macbookair@MacBooks-MacBook-Air.local>
2023-07-30 17:21:03 +0700

    feat: setup integration with firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getDatabase, ref, onValue, off } from 'firebase/database';
 
+import { initializeApp } from 'firebase/app';
 import HomePage from '@/components/pages/Home';
 
 export default function Home() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    if (!initializeApp?.apps?.length) {
+      initializeApp({
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MESAUREMENT_ID
+      });
+    }
+
+    const database = getDatabase();
+    const databaseRef = ref(database, 'incomes');
+
+    const onDataChange = snapshot => {
+      setData(snapshot.val());
+    };
+    onValue(databaseRef, onDataChange);
+
+    return () => off(databaseRef, onDataChange);
+  }, []);
+
   return <HomePage />;
 }
