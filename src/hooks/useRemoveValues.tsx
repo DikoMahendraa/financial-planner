d6d5f206a512595c29703d23cac4771bf78ebb30
Diko Mahendra <macbookair@MacBooks-MacBook-Air.local>
2023-08-08 14:35:16 +0700

    add: hooks for remove data

---
+++ src/hooks/useRemoveValues.tsx
@@ -0,0 +1,33 @@
+import { useRef, useState } from 'react';
+import { child, ref, remove } from 'firebase/database';
+import { database } from '@/services/firebaseApp';
+
+export default function useRemoveValues() {
+  const [isLoading, setIsLoading] = useState(true);
+  const success = useRef(false);
+  const error = useRef(null);
+  const data: any = useRef(null);
+
+  const removeValue = async (path: string, values: any) => {
+    setIsLoading(true);
+    try {
+      const rootReference = ref(database);
+      const dbPath = child(rootReference, path);
+      await remove(dbPath);
+
+      success.current = true;
+    } catch (errorMsg: any) {
+      error.current = errorMsg.message;
+    }
+
+    setIsLoading(false);
+  };
+
+  return {
+    removeValue,
+    isLoading,
+    success: success.current,
+    error: error.current,
+    data: data.current
+  };
+}
