cef69bef235f69c0de06e837d01197f0c9da32b3
Diko Mahendra <diko.dev99@gmail.com>
2023-09-03 23:47:33 +0700

    feat: add meta

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MNavigation from '@/components/molecules/Navigation';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 type PropsMainLayout = {
   children: string | JSX.Element | JSX.Element[] | React.ReactNode;
@@ -15,6 +16,14 @@ export default function MainLayout(props: PropsMainLayout) {
 
   return (
     <main>
+      <Head>
+        <title>Financial planner | Homepage</title>
+        <meta
+          name="financial planner"
+          content="financial planner is web application for calculate your balanced salary, within a lot of variant/method/type"
+          key="financial"
+        />
+      </Head>
       <div className="h-screen bg-aero-blue md:w-[480px] w-full m-auto">
         {children}
         {hideNavigation && <MNavigation />}
