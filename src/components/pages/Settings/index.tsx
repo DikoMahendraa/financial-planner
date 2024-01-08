ed9bc2b7d88f05851aee0a8649b67553b3e6b331
Diko <diko.dev99@gmail.com>
2024-01-08 10:30:34 +0700

    fix: react hydration - add use client

--- src/components/pages/Settings/index.tsx
+++ src/components/pages/Settings/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 
 import AGap from '@/components/atoms/Gap';
@@ -28,33 +30,27 @@ export default function PageSettings() {
 
   return (
     <div className="h-screen px-4 pt-4">
-      <div
-        onClick={() => {}}
-        className="bg-main-white cursor-pointer rounded-xl border-2 border-vampire-black px-4 py-3 flex items-center justify-between"
-      >
-        <div>
+      <div className="bg-main-white cursor-pointer rounded-xl border-2 border-vampire-black px-4 py-3 flex items-center justify-between">
+        <button onClick={() => {}}>
           <p className="font-light text-sm">Profile</p>
           <p>
             Hi, <strong className="capitalize">{username}</strong>
           </p>
-        </div>
+        </button>
         <ICAvatarProfile />
       </div>
 
       <AGap height={10} />
       <div className="bg-main-white rounded-xl p-4 border-2 border-vampire-black flex items-center justify-between">
         <p className="font-semibold">Atur Tema</p>
-        <div
-          className="w-11 h-11 border-2 rounded-sm border-vampire-black bg-aero-blue cursor-pointer"
-          onClick={() => {}}
-        />
+        <div className="w-11 h-11 border-2 rounded-sm border-vampire-black bg-aero-blue cursor-pointer" />
       </div>
 
       <AGap height={10} />
       <div className="bg-main-white rounded-xl py-5 px-4 border-2 border-vampire-black flex items-center justify-between">
         <p className="font-semibold">Export Data</p>
 
-        <div className="cursor-pointer" onClick={() => {}}>
+        <div className="cursor-pointer">
           <ICDownload />
         </div>
       </div>
